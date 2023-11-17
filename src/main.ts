dc4ce828f67aa2b915e6f9129f36a07361ef56d1
cairnsj <51908793+cairnsj@users.noreply.github.com>
2023-11-17 10:28:58 +0000

    Add ast reader, to generate diagram objects

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+//
+// Copyright (c) James Killick and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+//
+
+import Parse from './SequenceDiagram'
+
+export { Parse }
