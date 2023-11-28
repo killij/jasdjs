10133ca68cc079d5a8e6a9b38b59f301544d8ca3
killij <51908793+killij@users.noreply.github.com>
2023-11-28 13:10:42 +0000

    Delete all code

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-//
-// Copyright (c) James Killick and contributors. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for details.
-//
-
-import Parse from './SequenceDiagramParser'
-import Renderer from './SequenceDiagramRenderer'
-
-export { Parse, Renderer }
