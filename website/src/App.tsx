19bfbfba1a7eb387a8863c8c60af08b72a0edb55
killij <51908793+killij@users.noreply.github.com>
2023-11-28 12:57:55 +0000

    Switch to a HashRouter for gh pages

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Route, Routes, HashRouter } from 'react-router-dom'
 import Home from './Home'
 import TryItNow from './TryItNow'
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -6,13 +6,13 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 export default function () {
     return (
     <div className="h-100" style={{display: "flex", flexDirection: "column"}}>
-            <BrowserRouter>
+            <HashRouter>
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/index" element={<Home />} />
                     <Route path="/tryitnow" element={<TryItNow />} />
                 </Routes>
-            </BrowserRouter>
+            </HashRouter>
         {/* <div style={{backgroundColor: "red"}}>
         </div> */}
         {/* <div style={{display: "flex", flexDirection: "row", flexGrow: 1}}>
