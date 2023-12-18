82626719441be3055a997e28be567336f69f4301
killij <51908793+killij@users.noreply.github.com>
2023-12-18 18:07:26 +0000

    Remove the hompage, it's pretty superfluous atm (#26)

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,5 +1,4 @@
 import { Route, Routes, HashRouter } from 'react-router-dom'
-import Home from './Home'
 import TryItNow from './TryItNow'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
@@ -9,9 +8,8 @@ const App = () => {
         <div className="page-wrapper">
             <HashRouter>
                 <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/index" element={<Home />} />
-                    <Route path="/tryitnow" element={<TryItNow />} />
+                    <Route path="/" element={<TryItNow />} />
+                    <Route path="/index" element={<TryItNow />} />
                 </Routes>
             </HashRouter>
         </div>
